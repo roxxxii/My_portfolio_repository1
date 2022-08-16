eefc5cc6ea18748910295ea6f728509161aa453b
roxxxii <110385254+roxxxii@users.noreply.github.com>
2022-08-16 13:38:59 +0300

    Task 2

---
+++ pages/dashboard.py
@@ -0,0 +1,29 @@
+from pages.base_page import BasePage
+
+
+class Dashboard(BasePage):
+
+
+Dev_team_contact_xpath= //*[1][name()='a'],
+
+Polski_xpath= //*[@class="MuiListItemText-root"],
+
+Shortcuts_xpath= //div[2]/div/div/h2,
+
+Main_page_xpath= ul:nth-child(2) > div:nth-child(1),
+
+Add_players-xpath= //a[starts-with(@href, '/en/players/add')],
+
+Events_count_xpath=  //*[text()="Events count"],
+
+Scouts_panel_xpath=  #__next > div.jss41 > header,
+
+Players_count_xpath=  #__next > div.jss41 > main > div:nth-child(2) > div,
+
+Activity_xpath=  //*[@id="__next"]/div[1]/main/div[3]/div[3],
+
+Matches_count_xpath=  //*[text()="Matches count"],
+
+
+pass
+
